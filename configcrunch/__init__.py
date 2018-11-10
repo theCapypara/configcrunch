3935a74e48b2584bcb4c99b836cfb4d2f27c07c4
Parakoopa <parakoopa@live.de>
2018-11-10 18:47:30 +0100

    __init__ imports

--- configcrunch/__init__.py
+++ configcrunch/__init__.py
@@ -1,3 +1,8 @@
+# Public classes and functions
+from .abstract import YamlConfigDocument, DocReference
+from .merger import resolve_and_merge, load_subdocument
+
+
 # Constants
 REF = "$ref"
 REMOVE = "$remove"
