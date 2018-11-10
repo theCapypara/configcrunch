2c7df8be863a055ab4629e6bd9e35041052c6d5f
Parakoopa <parakoopa@live.de>
2018-11-10 18:42:12 +0100

    Init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='configcrunch',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'schema >= 0.6',
+        'pyyaml >= 3.13',
+    ]
+)
