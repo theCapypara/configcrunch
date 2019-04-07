98dc6a19729f41a5556614db11bffa27598677ce
Parakoopa <parakoopa@live.de>
2019-04-07 23:12:16 +0200

    Changes to setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,15 @@ setup(
     name='configcrunch',
     version='0.1',
     packages=find_packages(),
-    include_package_data=True,
+    description='TODO',  # TODO
+    long_description='TODO',  # TODO
     install_requires=[
         'schema >= 0.6',
         'pyyaml >= 3.13',
         'jinja2 >= 2.10'
+    ],
+    # TODO
+    classifiers=[
+        'Programming Language :: Python',
     ]
 )
