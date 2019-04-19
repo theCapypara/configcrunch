1f1471d4fe65e89e59a9e90830b22ab0017855f9
Parakoopa <parakoopa@live.de>
2019-04-19 20:20:52 +0200

    Security update to Jinja2 2.10.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     install_requires=[
         'schema >= 0.6',
         'pyyaml >= 5.1',
-        'jinja2 >= 2.10'
+        'jinja2 >= 2.10.1'
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
