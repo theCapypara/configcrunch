86f49c941294c91dde03218aaa70abbfe747f752
Marco Köpcke <marco.koepcke@tudock.de>
2021-12-14 18:33:59 +0100

    Test cases for the internal_* methods

--- configcrunch/tests/acceptance/internal_test.py
+++ configcrunch/tests/acceptance/internal_test.py
@@ -1,39 +1,75 @@
-from configcrunch import load_multiple_yml
+import unittest
+
 from configcrunch.tests.fixtures.documents import Base
-from configcrunch.tests.acceptance.testcases import ConfigcrunchTestCase, deep_sort
 
 
-class InternalTest(ConfigcrunchTestCase):
-    @classmethod
-    def fixture_name(cls):
-        return 'internal'
+class InternalTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.doc = Base.from_dict({
+            "more": {
+                "key_before": "value_before"
+            }
+        })
 
     def test_internal_get_before_freeze(self):
-        self.fail("todo")
+        self.assertEquals({"key_before": "value_before"}, self.doc.internal_get("more"))
+        self.assertEquals(None, self.doc.internal_get("doesnt_exist"))
 
     def test_internal_set_before_freeze(self):
-        self.fail("todo")
+        self.doc.internal_set("more", "new_more")
+        self.doc.internal_set("new_key", "new_new_key")
+        self.assertEquals("new_more", self.doc.internal_get("more"))
+        self.assertEquals("new_new_key", self.doc.internal_get("new_key"))
 
     def test_internal_contains_before_freeze(self):
-        self.fail("todo")
+        self.assertTrue(self.doc.internal_contains("more"))
+        self.assertFalse(self.doc.internal_contains("doesnt_exist"))
 
     def test_internal_delete_before_freeze(self):
-        self.fail("todo")
+        self.doc.internal_delete("more")
+        self.assertFalse(self.doc.internal_contains("more"))
+        # Deleting must work without an error
+        self.doc.internal_delete("doesnt_exist")
 
     def test_internal_access_before_freeze(self):
-        self.fail("todo")
+        with self.doc.internal_access():
+            self.doc["more"] = "new_more"
+            self.doc["new_key"] = "new_new_key"
+            self.doc.internal_set("internal_new_key", "new_internal_new_key")
+        self.assertEquals("new_more", self.doc.internal_get("more"))
+        self.assertEquals("new_new_key", self.doc.internal_get("new_key"))
+        self.assertEquals("new_internal_new_key", self.doc.internal_get("internal_new_key"))
 
     def test_internal_get_after_freeze(self):
-        self.fail("todo")
+        self.doc.freeze()
+        self.assertEquals({"key_before": "value_before"}, self.doc.internal_get("more"))
+        self.assertEquals(None, self.doc.internal_get("doesnt_exist"))
 
     def test_internal_set_after_freeze(self):
-        self.fail("todo")
+        self.doc.freeze()
+        self.doc.internal_set("more", "new_more")
+        self.doc.internal_set("new_key", "new_new_key")
+        self.assertEquals("new_more", self.doc.internal_get("more"))
+        self.assertEquals("new_new_key", self.doc.internal_get("new_key"))
 
     def test_internal_contains_after_freeze(self):
-        self.fail("todo")
+        self.doc.freeze()
+        self.assertTrue(self.doc.internal_contains("more"))
+        self.assertFalse(self.doc.internal_contains("doesnt_exist"))
 
     def test_internal_delete_after_freeze(self):
-        self.fail("todo")
+        self.doc.freeze()
+        self.doc.internal_delete("more")
+        self.assertFalse(self.doc.internal_contains("more"))
+        # Deleting must work without an error
+        self.doc.internal_delete("doesnt_exist")
 
     def test_internal_access_after_freeze(self):
-        self.fail("todo")
+        self.doc.freeze()
+        with self.doc.internal_access():
+            self.doc["more"] = "new_more"
+            self.doc["new_key"] = "new_new_key"
+            self.doc.internal_set("internal_new_key", "new_internal_new_key")
+        self.assertEquals("new_more", self.doc.internal_get("more"))
+        self.assertEquals("new_new_key", self.doc.internal_get("new_key"))
+        self.assertEquals("new_internal_new_key", self.doc.internal_get("internal_new_key"))
