571f4b5885bb9fe0712ccab67c5acbaac8b37f3d
Marco Köpcke <marco.koepcke@tudock.de>
2022-07-27 12:58:37 +0200

    Drop support for Python 3.6

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ setup(
         'Programming Language :: Rust',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
