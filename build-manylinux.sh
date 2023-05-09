c491d8e1d5a92c014112f1524ab18c3c43bbaaa6
Marco Köpcke <marco.koepcke@tudock.de>
2023-05-09 16:59:05 +0200

    Use subshell for manylinux rust install

--- build-manylinux.sh
+++ build-manylinux.sh
@@ -2,10 +2,11 @@
 set -ex
 
 mkdir -p /build-rust
-cd /build-rust
-curl -O https://static.rust-lang.org/dist/rust-1.69.0-x86_64-unknown-linux-gnu.tar.gz
-tar -xf rust-1.69.0-x86_64-unknown-linux-gnu.tar.gz
-cd rust-1.69.0-x86_64-unknown-linux-gnu
-./install.sh --components=rustc,cargo,rust-std-x86_64-unknown-linux-gnu
-
-#source $HOME/.cargo/env
+(
+    cd /build-rust
+    curl -O https://static.rust-lang.org/dist/rust-1.69.0-x86_64-unknown-linux-gnu.tar.gz
+    tar -xf rust-1.69.0-x86_64-unknown-linux-gnu.tar.gz
+    cd rust-1.69.0-x86_64-unknown-linux-gnu
+    ./install.sh --components=rustc,cargo,rust-std-x86_64-unknown-linux-gnu
+    #source $HOME/.cargo/env
+)
