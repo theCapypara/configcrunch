99da4d18c7ea072f642aace563bc14f300465b8a
Marco Köpcke <marco.koepcke@tudock.de>
2023-05-09 17:02:33 +0200

    Bump down rust version

--- build-manylinux.sh
+++ build-manylinux.sh
@@ -4,9 +4,9 @@ set -ex
 mkdir -p /build-rust
 (
     cd /build-rust
-    curl -O https://static.rust-lang.org/dist/rust-1.69.0-x86_64-unknown-linux-gnu.tar.gz
-    tar -xf rust-1.69.0-x86_64-unknown-linux-gnu.tar.gz
-    cd rust-1.69.0-x86_64-unknown-linux-gnu
+    curl -O https://static.rust-lang.org/dist/rust-1.60.0-x86_64-unknown-linux-gnu.tar.gz
+    tar -xf rust-1.60.0-x86_64-unknown-linux-gnu.tar.gz
+    cd rust-1.60.0-x86_64-unknown-linux-gnu
     ./install.sh --components=rustc,cargo,rust-std-x86_64-unknown-linux-gnu
     #source $HOME/.cargo/env
 )
