4663bebfcda3a40c4503008e02aa568c3f09b7e8
Marco Köpcke <marco.koepcke@tudock.de>
2023-05-09 16:07:47 +0200

    manylinux: Install Rust manually -- components

--- build-manylinux.sh
+++ build-manylinux.sh
@@ -6,6 +6,6 @@ cd /build-rust
 curl -O https://static.rust-lang.org/dist/rust-1.69.0-x86_64-unknown-linux-gnu.tar.gz
 tar -xf rust-1.69.0-x86_64-unknown-linux-gnu.tar.gz
 cd rust-1.69.0-x86_64-unknown-linux-gnu
-./install.sh
+./install.sh --components=rustc,cargo,rust-std-x86_64-unknown-linux-gnu
 
 source $HOME/.cargo/env
